fa1ffa591c52d307f4e9882fa91f185bf1c7d426
wuyiyao <532117848@qq.com>
2021-07-22 14:07:00 +0800

    originCode

--- push.sh
+++ push.sh
@@ -3,9 +3,23 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git config --global user.name "wuyiyao"
-git config --global user.email "532117848@qq.com"
+# git config --global user.name "wuyiyao"
+# git config --global user.email "532117848@qq.com"
+# git init
+# git add -A
+# git commit -m "first commit"
+# git push -f git@github.com:github.com/Zombie521/21sBlogImg master:master
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='originCode'
+  githubUrl=git@github.com:Zombie521/21sBlogImg
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://wuyiyao:${GITHUB_TOKEN}@github.com/Zombie521/21sBlogImg
+  git config --global user.name "wuyiyao"
+  git config --global user.email "532117848@qq.com"
+fi
 git init
 git add -A
-git commit -m "first commit"
-git push -f git@github.com:github.com/Zombie521/21sBlogImg.git master:master
+git commit -m "${msg}"
+git push -f $githubUrl master:master # 推送到仓库github master分支
