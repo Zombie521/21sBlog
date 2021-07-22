5563ae3c254e1c882803093a2af9f8e6b97a196a
wuyiyao <532117848@qq.com>
2021-07-22 14:03:39 +0800

    first commit

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+git config --global user.name "wuyiyao"
+git config --global user.email "532117848@qq.com"
+git init
+git add -A
+git commit -m "first commit"
+git remote add origin 
+git push -f https://github.com/Zombie521/21sBlogImg.git master:master
